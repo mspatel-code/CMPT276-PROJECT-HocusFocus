1417b261e0f0780a75c67f7658932706fb289c31
jrang188 <jrang@sfu.ca>
2021-07-09 16:01:25 -0700

    added delete and refactor

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -63,26 +63,25 @@ public class Main {
 
   @GetMapping("/signup")
   public String getSignupForm(Map<String, Object> model) {
-    adminInfo userInput = new adminInfo();
+    Users userInput = new Users();
     model.put("userInput", userInput);
     return "signup";
   }
 
   @PostMapping(path = "/signup", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
-  public String handleBrowserSingupSubmit(Map<String, Object> model, adminInfo userInput) throws Exception {
+  public String handleBrowserSingupSubmit(Map<String, Object> model, Users userInput) throws Exception {
     // save the user data into the database
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate(
           "CREATE TABLE IF NOT EXISTS userLogin (id serial, userName varchar(20), userType varchar(20), userPassword varchar(20))");
-      String sql = "INSERT INTO userLogin (userName,userType,userPassword) VALUES ('" + userInput.getuserName() + "' , '"
-          + userInput.getuserType() + "' , '" + userInput.getuserPassword() + "')";
+      String sql = "INSERT INTO userLogin (userName,userType,userPassword) VALUES ('" + userInput.getName() + "' , '"
+          + userInput.getType() + "' , '" + userInput.getPassword() + "')";
       stmt.executeUpdate(sql);
-      System.out.println(userInput.getuserName() + " " + userInput.getuserType() + " " + userInput.getuserPassword());
-      if(userInput.getuserType().equals("Admin")){
+      System.out.println(userInput.getName() + " " + userInput.getType() + " " + userInput.getPassword());
+      if (userInput.getType().equals("Admin")) {
         return "redirect:/admin";
-      }
-      else{
+      } else {
         return "redirect:/pomodoro";
       }
     } catch (Exception e) {
@@ -91,19 +90,53 @@ public class Main {
     }
   }
 
-  @GetMapping("/admin")
-  public String getAdminSuccess(Map<String, Object> model) {
+  @GetMapping("/login")
+  public String getLoginForm(Map<String, Object> model) {
+    Users userLogin = new Users();
+    model.put("userLogin", userLogin);
+    return "login";
+  }
+
+  @PostMapping(path = "/login", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+  public String getUserData(Map<String, Object> model, Users logInfo) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      String sql = "SELECT userName, userPassword, userType FROM userLogin";
+
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while (rs.next()) {
+        if (rs.getString("userName").equals(logInfo.getName())
+            && rs.getString("userPassword").equals(logInfo.getPassword())) {
+              
+          
+          if (rs.getString("userType").equals("Admin")) {
+            return "redirect:/admin";
+          } else {
+            return "redirect:/pomodoro";
+          }
+        }
+      }
+      return "error_login";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @GetMapping("/admin/{name}")
+  public String getAdminResult(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       ResultSet rs = stmt.executeQuery("SELECT * FROM userLogin");
 
-      ArrayList<String> output = new ArrayList<String>();
+      ArrayList<Users> output = new ArrayList<Users>();
       while (rs.next()) {
-        String usern = rs.getString("userName");
-        String id = rs.getString("id");
-        String usert = rs.getString("userType");
-
-        output.add(id + "  |  " + usern + "  |  " + usert);
+        Users user = new Users();
+        user.setName(rs.getString("userName"));
+        user.setId(rs.getInt("id"));
+        user.setType(rs.getString("userType"));
+        output.add(user);
       }
 
       model.put("records", output);
@@ -115,28 +148,19 @@ public class Main {
 
   }
 
-  
-/*   @GetMapping(path = "/authenticate")
-  public String getUserData(Map<String, Object> model, adminInfo adminInfo) {
-
+  @GetMapping("/delete/{pid}")
+  public String deleteUser(Map<String, Object> model, @PathVariable String pid) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      String sql = "SELECT user, password FROM databasename";
-      ResultSet rs2 = stmt.executeQuery(sql);
-      ArrayList<adminInfo> userData = new ArrayList<>();
-      while (rs2.next()) {
-        if (rs.getString("userName") == adminInfo.getuserName()) {
-          if(rs.getString("password") == adminInfo.userPassword()) {
-            return "pomodoro";
-          }
-        else{
-          return "error_login";
-        }
-      }
+      stmt.executeUpdate("DELETE FROM userLogin WHERE id=" + pid);
+      System.out.println("Successfully Deleted");
+
+      return "delete";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
     }
-  } */
-
-
+  }
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
---
+++ src/main/java/com/example/Users.java
@@ -0,0 +1,36 @@
+package com.example;
+
+public class Users {
+
+    private String name;
+    private String type;
+    private String password;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+    
