435b3da662d7f6b01ef47af873ccd0d5dc5971c9
jrang188 <jrang@sfu.ca>
2021-07-09 16:15:20 -0700

    revision:

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -124,7 +124,7 @@ public class Main {
     }
   }
 
-  @GetMapping("/admin/{name}")
+  @GetMapping("/admin")
   public String getAdminResult(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
@@ -161,6 +161,12 @@ public class Main {
       return "error";
     }
   }
+
+  @GetMapping("/logout")
+  public String getLogout(Map<String, Object> model) {
+    return "redirect:/pomodoro";
+  }
+
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
