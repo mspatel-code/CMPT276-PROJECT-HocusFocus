f805ec95e74d204cb5e305694eda4a07a5060edd
edoo-N <natanael.edward@gmail.com>
2021-08-06 12:57:09 -0700

    added more matrices on the report page

---
+++ src/main/java/com/example/Report.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Report{
+
+    private int total;
+    private float avg;
+    private int shortest;
+    private int longest;
+
+    public int getTotal(){
+        return total;
+    }
+    public void setTotal(int total){
+        this.total = total;
+    }
+    public float getAvg(){
+        return avg;
+    }
+    public void setAvg(float avg){
+        this.avg = avg;
+    }
+    public int getShortest(){
+        return shortest;
+    }
+    public void setShortest(int shortest){
+        this.shortest = shortest;
+    }
+    public int getLongest(){
+        return longest;
+    }
+    public void setLongest(int longest){
+        this.longest = longest;
+    }
+    
+    
+
+}
