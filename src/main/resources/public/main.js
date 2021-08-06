29976ffa0e6a9df2729fcefea91369373ba393a5
jrang188 <jrang@sfu.ca>
2021-08-05 22:05:24 -0700

    displayed web api to the landing page

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -172,3 +172,22 @@ function deleteCheck(e)
 }
 
 
+//Quotes API
+let url = "https://type.fit/api/quotes"
+
+
+function fetchData() {
+  fetch(url).then(response => {
+    return response.json();
+  }).then(data => {
+    console.log("I am called")
+    let i = Math.floor(Math.random() * 1644)
+    author = data[i].author;
+    quote = data[i].text;
+    document.getElementById("quote").innerHTML = quote
+    document.getElementById("author").innerHTML = author
+  });
+}
+
+fetchData()
+setInterval(fetchData, 120000);
